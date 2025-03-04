5eea0b24383c697348661c03faf724154d860a9c
Dan Dresser <ddresser05@gmail.com>
2025-03-04 13:51:35 -0600

    More robust error handling in main

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use anyhow::Result;
 use std::rc::Rc;
 
 mod db;
@@ -10,7 +9,7 @@ mod navigator;
 use navigator::*;
 mod ui;
 
-fn main() -> Result<()> {
+fn main() {
     let db = Rc::new(JiraDatabase::new("data/db.json".to_string()));
     let mut navigator = Navigator::new(db.clone());
 
@@ -19,15 +18,31 @@ fn main() -> Result<()> {
 
         match navigator.get_current_page() {
             Some(page) => {
-                page.draw_page()?;
+                if let Err(error) = page.draw_page() {
+                    println!("Error rendering page: {error}\nPress any key to continue...");
+                    wait_for_key_press();
+                }
+
                 let user_input = get_user_input();
-                if let Some(action) = page.handle_input(&user_input)? {
-                    navigator.handle_action(action)?;
+
+                match page.handle_input(&user_input) {
+                    Ok(action) => {
+                        if let Some(action) = action {
+                            if let Err(error) = navigator.handle_action(action) {
+                                println!(
+                                    "Error processing user input: {error}\nPress any key to continue..."
+                                );
+                                wait_for_key_press();
+                            }
+                        }
+                    }
+                    Err(error) => {
+                        println!("Error getting user input: {error}\nPress any key to continue...");
+                        wait_for_key_press();
+                    }
                 }
             }
             None => break,
         }
     }
-
-    Ok(())
 }
